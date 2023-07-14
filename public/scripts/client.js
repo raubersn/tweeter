1e944b0a2b105f4d7a99d1c2d5458f8abe466422
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-07-13 21:50:54 -0700

    Refactor how tweet data is submitted using jQuery/AJAX

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -28,42 +28,19 @@ return $(`<article class="tweet">
 };
 
 const renderTweets = (tweets) => {
-  const tweetElements = [];
-  
+  const $tweetElements = [];
+
   for (let tweet of tweets) {
-    tweetElements.push(createTweetElement(tweet));  
+    $tweetElements.push(createTweetElement(tweet));  
   }
 
-  $('#tweets-container').append(tweetElements);
+  $('#tweets-container').append($tweetElements);
 };
 
-
 $(document).ready(function() {
-// Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  $("#tweets-form").on("submit", function (event) {
+    event.preventDefault();
 
-  renderTweets(data);
+    $.post($("#tweets-form").attr('action'), $("#tweets-form").serialize());
+  })
 });
